0b72804704d013b7ef0249a751f4e569a77fb30b
kimhyeji <gpwl5589@gamil.com>
2023-04-19 20:37:27 +0900

    form을 사용해서 dom 조작하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import NoRecord from "./NoRecord";
 
 function App() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    form.name.value = form.name.value.trim();
+
+    if (form.name.value.length == 0) {
+      alert("이름을 입력해주세요.");
+      form.name.focus();
+      return;
+    }
+
+    form.age.value = form.age.value.trim();
+
+    if (form.age.value.length == 0) {
+      alert("나이를 입력해주세요.");
+      form.age.focus();
+      return;
+    }
+
+    const name = form.name.value;
+    const age = form.age.valueAsNumber;
+
+    alert(`이름 : ${name}, 나이 : ${age}`);
+
+    form.name.value = "";
+    form.age.value = "";
+
+    form.name.focus();
+  };
+
   return (
     <>
       <NoRecord />
+      <form onSubmit={onSubmit}>
+        <input type="text" name="name" placeholder="이름을 입력해주세요." />
+        <br />
+        <input type="number" name="age" placeholder="나이을 입력해주세요." />
+        <br />
+        <input type="submit" value="전송" />
+      </form>
     </>
   );
 }
--- src/NoRecord.js
+++ src/NoRecord.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 
 export default function NoRecord() {
-  const [no, setNo] = useState("");
   const [recordedNos, setRecordedNos] = useState([]);
 
-  const saveNo = () => {
-    if (no === "") {
+  const saveNo = (form) => {
+    form.no.value = form.no.value.trim();
+
+    if (form.no.value.length === 0) {
       alert("숫자를 입력해주세요.");
+      form.no.focus();
       return;
     }
 
-    setRecordedNos([...recordedNos, no]);
-    setNo("");
+    setRecordedNos([...recordedNos, form.no.value]);
+    form.no.value = "";
+    form.no.focus();
   };
 
   // const li = [1, 2, 3].map((el, index) => <li key={index}>{el}</li>);
@@ -22,14 +25,10 @@ export default function NoRecord() {
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          saveNo();
+          saveNo(e.target);
         }}
       >
-        <input
-          type="number"
-          value={no}
-          onChange={(e) => setNo(e.target.valueAsNumber)}
-        />
+        <input type="number" name="no" />
         <button type="submit">기록</button>
       </form>
       {/* 버전1 */}
