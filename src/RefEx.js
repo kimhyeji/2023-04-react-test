d2d6f140f3fea894454d2e851f0202fa1c3d1797
kimhyeji <gpwl5589@gamil.com>
2023-04-24 20:37:00 +0900

    배열에서 특정요소 수정

--- src/RefEx.js
+++ src/RefEx.js
@@ -4,7 +4,9 @@ export default function RefEx() {
   const noInputRef = useRef(null);
   const [no, setNo] = useState("");
 
-  const [recordedNos, setRecordedNos] = useState([5, 10, 15, 20, 5, 20, 10, 5]);
+  const [recordedNos, setRecordedNos] = useState([
+    5, 10, 15, 20, 5, 25, 5, 30, 5,
+  ]);
 
   const saveNo = () => {
     if (no === "") {
@@ -19,6 +21,13 @@ export default function RefEx() {
 
   const removeNo = (index) => {
     const newRecordedNos = recordedNos.filter((_, _index) => _index != index);
+    setRecordedNos(newRecordedNos);
+  };
+
+  const modifyNo = (index, newNo) => {
+    const newRecordedNos = recordedNos.map((el, _index) =>
+      _index == index ? newNo : el
+    );
     setRecordedNos(newRecordedNos);
   };
 
@@ -41,13 +50,16 @@ export default function RefEx() {
 
       <hr />
 
-      <h1>기록된 숫자 v1</h1>
+      <h1>기록된 숫자</h1>
       <ul>
         {recordedNos.map((el, index) => (
           <li key={index}>
-            <span style={{ width: 50, display: "inline-block" }}>{el}</span>
-            <span style={{ width: 50, display: "inline-block" }}>{index}</span>
+            <span style={{ width: 70, display: "inline-block" }}>{el}</span>
+            <span style={{ width: 70, display: "inline-block" }}>{index}</span>
             <button onClick={() => removeNo(index)}>삭제</button>
+            <button onClick={() => modifyNo(index, el + 1)}>+1</button>
+            <button onClick={() => modifyNo(index, el - 1)}>-1</button>
+            <button onClick={() => modifyNo(index, 0)}>0</button>
           </li>
         ))}
       </ul>
