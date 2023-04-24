47b3c0ff311909e7388a06b759dc002c3b24e2e7
kimhyeji <gpwl5589@gamil.com>
2023-04-24 20:20:19 +0900

    배열에서 특정요소를 삭제

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import { useState, useRef } from "react";
-import NoRecord from "./NoRecord";
-
-let AppCallCount = 0;
+import RefEx from "./RefEx";
 
 function App() {
-  AppCallCount++;
-  console.log(`App : ${AppCallCount}`);
-
-  const [no1, setNo1] = useState(0);
-  const no2Ref = useRef(0);
-
   return (
     <>
-      <button onClick={() => setNo1(no1 + 1)}>숫자 1 증가 : {no1}</button>
-      <button onClick={() => no2Ref.current++}>
-        숫자 2 증가 : {no2Ref.current}
-      </button>
+      <RefEx />
     </>
   );
 }
---
+++ src/RefEx.js
@@ -0,0 +1,56 @@
+import { useState, useRef } from "react";
+
+export default function RefEx() {
+  const noInputRef = useRef(null);
+  const [no, setNo] = useState("");
+
+  const [recordedNos, setRecordedNos] = useState([5, 10, 15, 20, 5, 20, 10, 5]);
+
+  const saveNo = () => {
+    if (no === "") {
+      alert("숫자를 입력해주세요.");
+      return;
+    }
+
+    setRecordedNos([...recordedNos, no]);
+    setNo("");
+    noInputRef.current.focus();
+  };
+
+  const removeNo = (index) => {
+    const newRecordedNos = recordedNos.filter((_, _index) => _index != index);
+    setRecordedNos(newRecordedNos);
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          saveNo();
+        }}
+      >
+        <input
+          type="number"
+          ref={noInputRef}
+          value={no}
+          onChange={(e) => setNo(e.target.valueAsNumber)}
+        />
+        <button type="submit">기록</button>
+      </form>
+
+      <hr />
+
+      <h1>기록된 숫자 v1</h1>
+      <ul>
+        {recordedNos.map((el, index) => (
+          <li key={index}>
+            <span style={{ width: 50, display: "inline-block" }}>{el}</span>
+            <span style={{ width: 50, display: "inline-block" }}>{index}</span>
+            <button onClick={() => removeNo(index)}>삭제</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
