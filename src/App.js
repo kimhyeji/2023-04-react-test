efda6fc5064db6a6e06c2c2c64aebd99a1442153
kimhyeji <gpwl5589@gamil.com>
2023-04-26 19:18:40 +0900

    알림기능구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
-import RefEx from "./RefEx";
+import React, { useState } from "react";
+import classnames from "https://cdn.skypack.dev/classnames";
+
+function NotifyOnce({ children }) {
+  const [visible, setVisible] = useState(false);
+  const [workDone, setWorkDone] = useState(false);
+
+  if (workDone == false) {
+    setTimeout(function () {
+      setVisible(true);
+    }, 1000);
+
+    setTimeout(function () {
+      setVisible(false);
+    }, 3000);
+
+    setWorkDone(true);
+  }
+
+  return (
+    <div
+      className={classnames(
+        "fixed transition-all right-[10px]",
+        {
+          "top-[-60px]": !visible,
+        },
+        {
+          "top-[10px]": visible,
+        }
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+function Alert({ color: color_, children }) {
+  const color = color_ ?? "white";
+
+  return (
+    <div className="alert alert-info shadow-lg">
+      <div className={`text-[${color}]`}>
+        <span>
+          <i className="fa-solid fa-circle-info"></i>
+        </span>
+        <span>{children}</span>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <>
-      <RefEx />
+      <NotifyOnce>
+        <Alert>안녕하세요</Alert>
+      </NotifyOnce>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, amet
+        mollitia impedit porro asperiores quidem voluptatum cumque saepe iusto
+        laudantium velit ad maiores nemo dolore ipsa delectus fuga ab quam.
+      </div>
     </>
   );
 }
